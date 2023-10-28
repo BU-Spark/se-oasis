6fc89b46890ec329666912ede0d97e7230734909
Harshitha Tumkur Kailasa Murthy <htk@Harshithas-MacBook-Pro.local>
2023-10-28 03:50:42 -0400

    Front end updated Homepage, Navbar and SignIn Pages

---
+++ client/src/components/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import React from "react";
+// import Navbr from "../Navbar";
+
+function HomePage() {
+    return (
+        <>
+            <Typography></Typography>
+        </>
+    );
+}
+
+export default HomePage;
---
+++ client/src/components/User/Signup/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function Signup() {
+    return (
+        <>
+            <Typography> Hello World </Typography>
+        </>
+    );
+}
+
+export default Signup;

c5e40eb7a6035ac57fe567f53113c3702a3b594d
Vishwas Bhaktavatsala <vishwasb@bu.edu>
2023-11-18 01:19:33 -0500

    One UserRegistered Flow complete

--- core/UserRegisteredEventListner/userRegister/index.js
+++ core/UserRegisteredEventListner/userRegister/index.js
@@ -1,13 +1,29 @@
-const functions = require("@google-cloud/functions-framework");
+const { Firestore } = require("@google-cloud/firestore");
 
-// Register a CloudEvent function with the Functions Framework
-functions.cloudEvent("userRegisteredEventListner", (cloudEvent) => {
-    console.log(cloudEvent.specversion);
-    console.log(cloudEvent.type);
-    console.log(cloudEvent.source);
-    console.log(cloudEvent.subject);
-    console.log(cloudEvent.id);
-    console.log(cloudEvent.time);
-    console.log(cloudEvent.datacontenttype);
-    console.log(cloudEvent.data);
-});
+exports.userRegisteredEventListner = async (event, context) => {
+    const pubsubMessage = event.data;
+
+    const messageData = JSON.parse(
+        Buffer.from(pubsubMessage, "base64").toString()
+    );
+
+    await writeToFirestore("users", messageData);
+};
+
+async function writeToFirestore(collectionName, messageData) {
+    try {
+        const firestore = new Firestore();
+
+        const documentRef = firestore.collection(collectionName).doc();
+
+        // Write data to Firestore
+        await documentRef.set({
+            ...messageData,
+            timestamp: new Date(),
+        });
+
+        console.log(`Data written to Firestore: ${messageData}`);
+    } catch (error) {
+        console.error("Error writing to Firestore:", error);
+    }
+}

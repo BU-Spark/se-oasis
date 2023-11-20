e4ee6b928ac437fc33a3fa009c30f5ceb0689d33
Vishwas Bhaktavatsala <vishwasb@bu.edu>
2023-11-20 01:49:05 -0500

    updated the login code

--- core/UserLoginEventListner/UserLogin/index.js
+++ core/UserLoginEventListner/UserLogin/index.js
@@ -1,6 +1,6 @@
 const { Firestore } = require("@google-cloud/firestore");
 
-exports.UserLoginEventListner = async (event, context) => {
+exports.userLoginEventListner = async (event, context) => {
     const pubsubMessage = event.data;
 
     const messageData = JSON.parse(
@@ -11,14 +11,23 @@ exports.UserLoginEventListner = async (event, context) => {
 };
 
 async function readAndUpdateFirestore(collectionName, messageData) {
-    const usersCollection = db.collection(collectionName);
-    const userData = await usersCollection
-        .where("email", "==", messageData.email)
-        .limit(1)
-        .get();
+    try {
+        console.log({ collectionName, email: messageData.email });
+        const firestore = new Firestore();
 
-    await db
-        .collection(collectionName)
-        .doc(userData.id)
-        .set({ lastLogin: new Date() }, { merge: true });
+        const usersCollection = firestore.collection(collectionName);
+        const userDataSnapshot = await usersCollection
+            .where("email", "==", messageData.email)
+            .limit(1)
+            .get();
+
+        userDataSnapshot.forEach(async (userData) => {
+            await firestore
+                .collection(collectionName)
+                .doc(userData.id)
+                .set({ lastLogin: new Date() }, { merge: true });
+        });
+    } catch (error) {
+        console.error("Error writing to Firestore:", error);
+    }
 }

f2d3360ea79eadbcb290f96a151cf096c406041a
Harshitha Tumkur Kailasa Murthy <htk@crc-dot1x-nat-10-239-131-62.bu.edu>
2023-11-20 16:44:34 -0500

    update footer

--- client/src/components/Footer/getinTouch.tsx
+++ client/src/components/Footer/getinTouch.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 interface GetInTouchProps {
     heading: string;
@@ -7,9 +8,19 @@ interface GetInTouchProps {
 }
 
 const GetInTouch: React.FC<GetInTouchProps> = ({ heading, message, email }) => {
+    const navigate = useNavigate();
+
+    const handleAboutClick = () => {
+        navigate("/about");
+    };
     return (
         <>
-            <h2 className="display-4 pb-3 text-center">{heading}</h2>
+            <h2
+                className="display-4 pb-3 text-center"
+                onClick={() => handleAboutClick()}
+            >
+                {heading}
+            </h2>
             <p className="lead text-center pb-3">
                 {message},{" "}
                 <a className="text-decoration-none" href={`mailto:${email}`}>

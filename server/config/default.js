bffb6fe3db7f6bf5ac2e56d17af5f876343b42e9
Vishwas Bhaktavatsala <vishwasb@bu.edu>
2023-12-22 00:08:23 -0500

    development

--- server/config/default.js
+++ server/config/default.js
@@ -2,7 +2,7 @@ module.exports = {
     app: {
         name: "Oasis Wellbeing Backend Server",
         connection: {
-            host: process.env.SERVER_HOST || "127.0.0.1",
+            host: process.env.SERVER_HOST || "0.0.0.0",
             port: process.env.SERVER_PORT || "5675",
         },
     },
